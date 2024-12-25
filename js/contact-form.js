ebb919b04b368b8f6b9c26b3839efe8a32bc0580
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-12-25 18:02:35 +0200

    WIP

--- js/contact-form.js
+++ js/contact-form.js
@@ -19,6 +19,6 @@ function sendForm() {
         window.location.href = newPath;
     } else {
         document.getElementById('result').textContent =
-            'Invalid e-mail. Please enter a valid e-mail address.';
+            'Geçersiz e-posta. Lütfen geçerli bir e-posta adresi girin.';
     }
 }
